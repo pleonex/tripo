0a0782552bb5fcb8b62182f75f4ec7c48425a67f
Benito Palacios Sanchez <pleonex@protonmail.com>
2024-06-09 18:00:03 +0200

    :wrench: Update to .NET 8.0

--- Tripo/MauiProgram.cs
+++ Tripo/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+﻿using CommunityToolkit.Maui;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace PleOps.Tripo;
 
@@ -9,6 +10,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
 			.UseSkiaSharp(true) // for Mapsui
 			.ConfigureFonts(fonts =>
 			{
